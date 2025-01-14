23b479f8610a11c10d5c64334c59e18ca16185ef
Patrick Her <renfu.her@gmail.com>
2025-01-14 12:22:53 +0800

    create 6 files

---
+++ database/migrations/2025_01_14_082604_create_test_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('test', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable()->comment('姓名');
+            $table->string('email')->nullable()->comment('電子郵件');
+            $table->string('phone')->nullable()->comment('電話');
+            $table->string('address')->nullable()->comment('地址');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('test');
+    }
+};
